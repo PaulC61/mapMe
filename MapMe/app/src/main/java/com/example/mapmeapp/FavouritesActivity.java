5f15021a2bcde7761af9f52c4331fee67bfd43fb
PaulC61 <paulclarke61@gmail.com>
2020-05-24 14:58:59 +0200

    Favourites list successfully generated into list view using Array Adapter in FavouritesActivity

--- MapMe/app/src/main/java/com/example/mapmeapp/FavouritesActivity.java
+++ MapMe/app/src/main/java/com/example/mapmeapp/FavouritesActivity.java
@@ -2,6 +2,8 @@ package com.example.mapmeapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +20,10 @@ public class FavouritesActivity extends AppCompatActivity {
         Intent intent = getIntent();
         List<String> savedMarkerTitles = intent.getStringArrayListExtra(MapsActivity.EXTRA_MARKERS);
 
-        //loading markers
+        //loading markers into list using ArrayAdapter
+        ArrayAdapter<String> savedMarkerTitlesAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_2, savedMarkerTitles);
+        ListView favouritesListView = findViewById(R.id.favourites_list);
+        favouritesListView.setAdapter(savedMarkerTitlesAdapter);
     }
 }
